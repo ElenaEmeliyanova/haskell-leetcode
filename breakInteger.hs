82544bb7a0e10b49d3320df02d30499349f926c5
Elena Emeliyanova <85221256+ElenaEmeliyanova@users.noreply.github.com>
2021-06-09 22:18:57 +0200

    Update breakInteger.hs
    Implement display tree.

--- breakInteger.hs
+++ breakInteger.hs
@@ -1,16 +1,22 @@
 -- LeetCode Problem: Break Integer
+-- Runnable at https://replit.com/@dbizchallenge/BreakInteger-Haskell#main.hs
 
-data SumTree = Leaf Int | SumNode Int SumTree SumTree deriving Show
+import Data.Tree hiding (Tree)
+
+main :: IO ()
+main = return ()
+
+data Tree a = Leaf a | Branch a (Tree a) (Tree a) deriving Show
 
 breakInteger :: Int -> Int
 breakInteger 2 = 1
 breakInteger 3 = 2
 breakInteger x = treeProduct(toTree x)
 
-toTree :: Int -> SumTree
+toTree :: Int -> Tree Int
 toTree x 
   | leafValues x = Leaf x
-  | otherwise = SumNode x (toTree (partition x)) (toTree (partitionRest x))
+  | otherwise = Branch x (toTree (partition x)) (toTree (partitionRest x))
 
 leafValues x = x >= 2 && x <= 3
 
@@ -20,6 +26,17 @@ partition x
  
 partitionRest x = x - partition x
 
-treeProduct :: SumTree -> Int 
+treeProduct :: Tree Int -> Int 
 treeProduct (Leaf x) = x
-treeProduct (SumNode x l r) = (treeProduct l) * (treeProduct r)
+treeProduct (Branch x l r) = (treeProduct l) * (treeProduct r)
+
+-- Display Tree
+toPrintable :: Int -> Tree String
+toPrintable x 
+  | leafValues x = Leaf (show x)
+  | otherwise = Branch (show x) (toPrintable (partition x)) (toPrintable (partitionRest x))
+
+toDataTree (Leaf x) = Node x []
+toDataTree (Branch x l r) = Node x [toDataTree l, toDataTree r]
+
+printTree i = putStrLn $ drawTree (toDataTree (toPrintable i))
